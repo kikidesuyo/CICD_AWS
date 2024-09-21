8f856ce3d4d47c54a2524832b57970fd5865d6cf
林政人 <masato@hayashimasatonoMacBook-Pro.local>
2024-09-21 18:33:33 +0900

    modify

--- selenium_test.py
+++ selenium_test.py
@@ -14,6 +14,8 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')  # GPUを無効化
 options.add_argument('--remote-debugging-port=9222')  # デバッグポートを指定
+options.add_argument('--disable-software-rasterizer')  # ソフトウェアラスタライザを無効化
+options.add_argument('--window-size=1920,1080')  # ウィンドウサイズを指定
 
 # ドライバーのセットアップ
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
